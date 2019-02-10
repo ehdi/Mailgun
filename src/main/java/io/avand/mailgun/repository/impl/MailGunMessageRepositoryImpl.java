b1439e82367810fabc2a8606f71193d24a437afd
ehdi <i@ehdi.in>
2019-02-11 00:52:17 +0330

    Update to latest version

--- src/main/java/io/avand/mailgun/repository/impl/MailGunMessageRepositoryImpl.java
+++ src/main/java/io/avand/mailgun/repository/impl/MailGunMessageRepositoryImpl.java
@@ -1,44 +1,44 @@
 package io.avand.mailgun.repository.impl;
 
 import io.avand.mailgun.config.MailGunConfig;
-import io.avand.mailgun.repository.util.HeaderUtil;
 import io.avand.mailgun.repository.MailGunMessageRepository;
+import io.avand.mailgun.repository.util.HeaderUtil;
 import io.avand.mailgun.service.dto.request.MailGunSendMessageRequestDTO;
 import io.avand.mailgun.service.dto.response.MailGunSendMessageResponseDTO;
+import org.apache.http.entity.ContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Repository
 public class MailGunMessageRepositoryImpl implements MailGunMessageRepository {
-
     private final Logger log = LoggerFactory.getLogger(MailGunMessageRepositoryImpl.class);
-
     private final MailGunConfig mailGunConfig;
 
     public MailGunMessageRepositoryImpl(MailGunConfig mailGunConfig) {
         this.mailGunConfig = mailGunConfig;
     }
 
-    @Override
     public ResponseEntity<MailGunSendMessageResponseDTO> send(MailGunSendMessageRequestDTO requestDTO) {
-        log.debug("Request to send message via repository : {}", requestDTO);
-        UriComponentsBuilder builder =
-                UriComponentsBuilder.fromUriString("https://api.mailgun.net/v3/" + mailGunConfig.getDomain() + "/messages")
-                        .queryParam("from", mailGunConfig.getFrom())
-                        .queryParam("to", requestDTO.getTo())
-                        .queryParam("subject", requestDTO.getSubject())
-                        .queryParam("text", requestDTO.getText())
-                        .queryParam("html", requestDTO.getHtml());
+        this.log.debug("Request to send message via repository : {}", requestDTO);
+        MultiValueMap<String, Object> parts = new LinkedMultiValueMap();
+        parts.add("from", requestDTO.getFromName() + " <" + this.mailGunConfig.getFrom() + ">");
+        parts.add("to", requestDTO.getTo());
+        parts.add("subject", requestDTO.getSubject());
+        parts.add("text", requestDTO.getText());
+        parts.add("html", requestDTO.getHtml());
+        if (requestDTO.getAttachment() != null) {
+            parts.add("attachment", new FileSystemResource(requestDTO.getAttachment()));
+        }
 
-        HttpEntity request =
-                new HttpEntity<>(HeaderUtil.createHeader("api",mailGunConfig.getApiKey()));
-        return new RestTemplate()
-                .exchange(builder.build().toUri(), HttpMethod.POST,request,MailGunSendMessageResponseDTO.class);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity(parts, HeaderUtil.createHeader("api", this.mailGunConfig.getApiKey(), requestDTO.getAttachment() == null ? null : ContentType.MULTIPART_FORM_DATA.toString()));
+        return (new RestTemplate()).exchange("https://api.mailgun.net/v3/" + this.mailGunConfig.getDomain() + "/messages", HttpMethod.POST, requestEntity, MailGunSendMessageResponseDTO.class, new Object[0]);
     }
 }
--- src/main/java/io/avand/mailgun/repository/impl/MailGunRouteRepositoryImpl.java
+++ src/main/java/io/avand/mailgun/repository/impl/MailGunRouteRepositoryImpl.java
@@ -21,66 +21,34 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Repository
 public class MailGunRouteRepositoryImpl implements MailGunRouteRepository {
-
     private final Logger log = LoggerFactory.getLogger(MailGunRouteRepositoryImpl.class);
-
     private final MailGunConfig mailGunConfig;
 
     public MailGunRouteRepositoryImpl(MailGunConfig mailGunConfig) {
         this.mailGunConfig = mailGunConfig;
     }
 
-    @Override
     public ResponseEntity<MailGunCreateRouteResponseDTO> createRoute(MailGunCreateRouteRequestDTO requestDTO) {
-        log.debug("Request to create route via repository : {}", requestDTO);
-        LinkedMultiValueMap<String, String> valueMap = new LinkedMultiValueMap<>();
+        this.log.debug("Request to create route via repository : {}", requestDTO);
+        LinkedMultiValueMap<String, String> valueMap = new LinkedMultiValueMap();
         valueMap.add("priority", "0");
         valueMap.add("description", requestDTO.getDescription());
         valueMap.add("expression", "match_recipient('" + requestDTO.getMatchRecipient() + "')");
         valueMap.add("action", "forward('" + requestDTO.getForwardTo() + "')");
-
-        HttpEntity<LinkedMultiValueMap<String, String>> request =
-                new HttpEntity<>(valueMap, HeaderUtil.createHeader(mailGunConfig.getUsername(), mailGunConfig.getApiKey()));
-        return new RestTemplate()
-                .exchange(
-                        "https://api.mailgun.net/v3/routes",
-                        HttpMethod.POST,
-                        request,
-                        MailGunCreateRouteResponseDTO.class
-                );
+        HttpEntity<LinkedMultiValueMap<String, String>> request = new HttpEntity(valueMap, HeaderUtil.createHeader(this.mailGunConfig.getUsername(), this.mailGunConfig.getApiKey(), (String)null));
+        return (new RestTemplate()).exchange("https://api.mailgun.net/v3/routes", HttpMethod.POST, request, MailGunCreateRouteResponseDTO.class, new Object[0]);
     }
 
-    @Override
     public ResponseEntity<MailGunGetRouteByIdResponseDTO> getRouteById(MailGunGetRouteByIdRequestDTO requestDTO) {
-        log.debug("Request to get route by id via repository : {}", requestDTO);
-
-        HttpEntity request =
-                new HttpEntity<>(HeaderUtil.createHeader(mailGunConfig.getUsername(), mailGunConfig.getApiKey()));
-        return new RestTemplate()
-                .exchange(
-                        "https://api.mailgun.net/v3/routes/" + requestDTO.getRouteId(),
-                        HttpMethod.GET,
-                        request,
-                        MailGunGetRouteByIdResponseDTO.class
-                );
+        this.log.debug("Request to get route by id via repository : {}", requestDTO);
+        HttpEntity request = new HttpEntity(HeaderUtil.createHeader(this.mailGunConfig.getUsername(), this.mailGunConfig.getApiKey(), (String)null));
+        return (new RestTemplate()).exchange("https://api.mailgun.net/v3/routes/" + requestDTO.getRouteId(), HttpMethod.GET, request, MailGunGetRouteByIdResponseDTO.class, new Object[0]);
     }
 
-    @Override
     public ResponseEntity<MailGunGetAllRouteResponseDTO> getAllRoute(MailGunGetAllRouteRequestDTO requestDTO) {
-        log.debug("Request to get all route via repository : {}", requestDTO);
-        UriComponentsBuilder builder =
-                UriComponentsBuilder.fromUriString("https://api.mailgun.net/v3/routes")
-                        .queryParam("skip", requestDTO.getSkip())
-                        .queryParam("limit", requestDTO.getLimit());
-
-        HttpEntity request =
-                new HttpEntity<>(HeaderUtil.createHeader(mailGunConfig.getUsername(), mailGunConfig.getApiKey()));
-        return new RestTemplate()
-                .exchange(
-                        builder.build().toUri(),
-                        HttpMethod.GET,
-                        request,
-                        MailGunGetAllRouteResponseDTO.class
-                );
+        this.log.debug("Request to get all route via repository : {}", requestDTO);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("https://api.mailgun.net/v3/routes").queryParam("skip", new Object[]{requestDTO.getSkip()}).queryParam("limit", new Object[]{requestDTO.getLimit()});
+        HttpEntity request = new HttpEntity(HeaderUtil.createHeader(this.mailGunConfig.getUsername(), this.mailGunConfig.getApiKey(), (String)null));
+        return (new RestTemplate()).exchange(builder.build().toUri(), HttpMethod.GET, request, MailGunGetAllRouteResponseDTO.class);
     }
 }
